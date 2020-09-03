6c6e4c38fe8f99dfbb3d166e510df9d6e72185ad
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-03 13:13:03 +0200

    everything

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('index.html'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
